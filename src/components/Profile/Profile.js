c2a5c81670e20f0c8460ef23499365bbe85eab75
artie021 <arthurfellype21@gmail.com>
2021-09-18 04:38:42 -0300

    Iniciando repositorio de portfolio pessoal desenvolvido em React

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div>
+            <h2> this is a profile</h2>
+        </div>
+    )
+}
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+        <div>
+            this is a portfolio
+        </div>
+    )
+}
+export default Portfolio
