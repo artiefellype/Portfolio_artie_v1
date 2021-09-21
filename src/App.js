c24dcdeab57495a6d722101095b0f01497dc3c08
artie021 <arthurfellype21@gmail.com>
2021-09-21 16:11:25 -0300

    Reiniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
-/* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
-import {Container, Grid} from "@material-ui/core";
-import Profile from "./components/Profile/Profile";
-import Header from "./components/Header/Header";
-import Portfolio from "./pages/Portfolio/Portfolio";
-import Resume from './pages/Resume/Resume';
-import Footer from './components/Footer/Footer';
-
-export default function App() {
-        return(
-        <div >
-                <Container>
-                        <Grid container>
-                                <Grid item xs={12} 
-                                      sm={12} 
-                                      lg={4} 
-                                      style={{backgroundColor:'red'}}>
-                                 <Profile/>
-                                 
-                                </Grid>
-                                <Grid item xs 
-                                      style={{backgroundColor: 'blue'}}>
-                                  <Header/>
-                                  <Router>
-                                        <Switch>
-                                          <Route path="/Portfolio">
-                                                <Portfolio/>
-                                          </Route>
-                                          <Route path="/Resume">
-                                                <Resume/>
-                                          </Route>
-                                        </Switch>
-                                        
-                                  </Router>        
-                                  
-                                  <Footer/>
-                                  
-                                </Grid>
-                        </Grid>
-                </Container>
-            
-        </div>
-        )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-  
-    
 
-
-
+export default App;
--- src/components/Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Profile() {
-    return (
-        <div>
-            <h2> this is a profile</h2>
-        </div>
-    )
-}
